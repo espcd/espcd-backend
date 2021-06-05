3f9af72f63962d080d2f9b08329befa42f86bbb5
lucasheld <lucasheld@hotmail.de>
2021-06-05 12:19:51 +0200

    allow only session users to connect to actioncable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      reject_unauthorized_connection unless cable_session_user
+    end
+
+    def cable_session_user
+      return nil unless request.params[:api_key]
+
+      User.find_by(session: request.params[:api_key])
+    end
   end
 end
